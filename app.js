47b1a59afed22c7cf0ff47caa619cbc0dca54c91
Eric Wallace <eric.t.wallace4@outlook.com>
2022-01-09 00:28:34 -0800

    added nav buttons, comments, add campground page

--- app.js
+++ app.js
@@ -14,28 +14,52 @@ mongoose.connect('mongodb://localhost:27017/YelpCampDB', {
         console.log(err);
     });
 
-const app = express();
-app.set('view engine', 'ejs');
+const app = express(); // Create server
+app.set('view engine', 'ejs'); // Allow use of ejs files
 app.set('views', path.join(__dirname, 'views'));
-
+app.use(express.urlencoded({ extended: true })); // Required for something....
 
 
 // Routes
+// Home page
 app.get('/', (req, res) => {
     res.render('home');
 });
 
+// Shows a list of all campgrounds in the database
+// Gets all the campgrounds from database
+// Renders the index page with the names of each camp in db
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', {campgrounds});
 });
 
+// Page to create a new campground to add to the database
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+// Page where the form from /campgrounds/new is posted too
+// Takes the information from form and creates a new campground
+// Saves the campground to the database
+// Then redirects the user to the show page of that new campground
+app.post('/campgrounds', async (req, res) => {
+    const newCamp = new Campground(req.body.campground);
+    await newCamp.save();
+    res.redirect(`/campgrounds/${newCamp._id}`);
+});
+
+// Page that shows the details of an individual campground
+// Takes the id from the url
+// Finds the corresponding campground with the matching id in the database
+// Renders the show page with the found campground information 
 app.get('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     const foundCamp = await Campground.findById(id);
     res.render('campgrounds/show', { foundCamp });
 });
 
+// Start up the server on port 3000, when connected notify
 app.listen(3000, () => {
     console.log('Server up, on port 3000');
 });
