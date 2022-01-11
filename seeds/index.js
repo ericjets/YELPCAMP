a5820255d141f983d859f277d553fac3ae651793
Eric Wallace <eric.t.wallace4@outlook.com>
2022-01-10 19:43:09 -0800

    updated camp model edited show page edited index page

--- seeds/index.js
+++ seeds/index.js
@@ -25,9 +25,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++){
         let randomNumber = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[randomNumber].city}, ${cities[randomNumber].state}`,
-            title: `${randomElement(descriptors)} ${randomElement(places)}`
+            title: `${randomElement(descriptors)} ${randomElement(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A sunt praesentium quo sequi iure architecto, eligendi nisi adipisci inventore qui iusto consequuntur fuga',
+            price
         });
         await camp.save();
     }
